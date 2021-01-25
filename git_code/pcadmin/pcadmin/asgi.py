7f05c2a337e9d8edb7b80a974f9a44b98c9f70ac
Mod_home_laptop <Mod_lp@lp.com>
2021-01-25 23:44:35 +0530

    whatever

---
+++ git_code/pcadmin/pcadmin/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pcadmin project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pcadmin.settings')
+
+application = get_asgi_application()
