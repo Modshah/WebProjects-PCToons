46cd320368df045b48de551d8ed09bf5d71ada00
guddu <mightygrv@gmail.com>
2021-01-26 14:38:59 +0530

    Added mysql connection in djnago

---
+++ git_code/pcadmin/padmin/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    likes = models.PositiveIntegerField(default=0)
+    image_path = models.CharField(max_length=500)
+    tags = models.CharField(max_length=500)
+
+
+class User(models.Model):
+    pass
+
